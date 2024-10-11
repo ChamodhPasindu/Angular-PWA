5ebada9dbc6e6dc5639dedeaedeaf6ae39966740
ChamodhPasindu <chamodhpasindu2000@gmail.com>
2024-10-11 11:05:37 +0530

    feat :add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FingerprintComponent } from './component/fingerprint/fingerprint.component';
+import { FaceDetectionComponent } from './component/face-detection/face-detection.component';
+import { QrCodeReaderComponent } from './component/qr-code-reader/qr-code-reader.component';
+import { BarcodeReaderComponent } from './component/barcode-reader/barcode-reader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FingerprintComponent,
+    FaceDetectionComponent,
+    QrCodeReaderComponent,
+    BarcodeReaderComponent
   ],
   imports: [
     BrowserModule,
